a14f85a04f64732ab5dde1f38144d7def477bd0a
El Hussain, Tarek <Tarek.El-Hussain@adesso.de>
2025-10-08 09:35:00 +0200

    WIP

--- src/components/atoms/Place/Place.tsx
+++ src/components/atoms/Place/Place.tsx
@@ -1,34 +1,97 @@
 'use client';
 
-import React, {useState} from "react";
+import React from "react";
+import {Button} from "@/components/ui/button";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 
-type statusTyp = 'frei' | 'belegt' | 'fehler' | 'wartung';
+type statusTyp = 'frei' | 'belegt' | 'fehler' | 'wartung'; // TODO: change to frei | belegt | reserviert
+type sizeTyp = 'small' | 'medium' | 'large';
 
 interface PlaceProps {
     placeName: string,
-    value: statusTyp,
+    status: statusTyp,
+    size: sizeTyp
 }
 
-const Place = ({placeName, value}: PlaceProps) => {
-    const [status, setStatus] = useState(value);
-
-    const getPlaceStyles = (status: statusTyp): string => {
+const Place = ({placeName, status, size}: PlaceProps): React.ReactElement => {
+    const setPlaceStyles = (status: statusTyp): string => {
         switch (status) {
             case 'frei':
                 return 'bg-green-900';
             case 'belegt':
-                return 'bg-grey-900';
+                return 'bg-gray-500';
             case 'fehler':
-                return 'bg-yellow-900';
+                return 'bg-red-900';
             case 'wartung':
                 return 'bg-purple-900';
         }
     }
 
+    const setSize = (size: sizeTyp): string => {
+        switch (size) {
+            case 'small':
+                return 'w-16';
+            case 'medium':
+                return 'w-24';
+            case 'large':
+                return 'w-30';
+        }
+    }
+
+    const onSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log("submit");
+    }
+
     return (
-        <div className={`h-8 w-8 text-white ${getPlaceStyles(status)}`}>
-            <span>{placeName}</span>
-        </div>
+        <Dialog>
+            <form>
+                <DialogTrigger asChild className='h-full p-0 hover:cursor-pointer hover:opacity-80'>
+                    <Tooltip>
+                        <TooltipTrigger asChild className='h-full p-0'>
+                            <Button variant="ghost" className='hover:cursor-pointer'>
+                                <div
+                                    className={`h-16 text-white flex flex-col justify-center items-center align-middle ${setPlaceStyles(status)} ${setSize(size)}`}>
+                                    <span>{placeName}</span>
+                                </div>
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <span>{size}</span>
+                        </TooltipContent>
+                    </Tooltip>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>{`Platz ${placeName} buchen`}</DialogTitle>
+                        <DialogDescription>
+                            Hier kannst du den Platz {placeName} buchen.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline" className='hover:cursor-pointer'>abbrechen</Button>
+                        </DialogClose>
+                        <Button type="submit" className='hover:cursor-pointer' onClick={onSubmit}>Buchen</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </form>
+        </Dialog>
+
     );
 }
 
--- src/components/organisms/Booking/Booking.tsx
+++ src/components/organisms/Booking/Booking.tsx
@@ -1,10 +1,21 @@
 import Place from "@/components/atoms/Place/Place";
 
+interface BookingProps {
+    places: Array<string>
+}
 
-const Booking = () => {
+const Booking = ({places}: BookingProps) => {
+
     return (
-        <div>
-            <Place placeName='01' value='frei'/>
+        <div className='grid grid-flow-col'>
+            <Place placeName='01' status='frei' size='small'/>
+            <Place placeName='02' status='belegt' size='medium'/>
+            <Place placeName='03' status='fehler' size='large'/>
+            <Place placeName='04' status='wartung' size='small'/>
+            <Place placeName='05' status='belegt' size='small'/>
+            <Place placeName='06' status='frei' size='small'/>
+            <Place placeName='07' status='belegt' size='small'/>
+            <Place placeName='08' status='frei' size='small'/>
         </div>
     );
 }
